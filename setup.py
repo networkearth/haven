d9d88c2fe1d5ac37f4d47edceb71735075877a06
erebuseternal <marcelsanders96@gmail.com>
2024-10-30 16:10:58 +0000

    wip

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ setup(
     author_email='marcelsanders96@gmail.com',
     packages=find_packages(include=['haven', 'haven*']),
     install_requires=[
-        'click==8.1.7',
-        'aws-cdk-lib==2.154.1',
-        'awswrangler==3.9.1',
-        'pyyaml==6.0.2',
+        'click',
+        'aws-cdk-lib',
+        'awswrangler',
+        'pyyaml',
+        'pylint',
     ],
     entry_points={
         'console_scripts': [
