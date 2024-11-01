8429c1b4896777bf93429efb07caee512b8fddfb
erebuseternal <marcelsanders96@gmail.com>
2024-11-01 12:55:30 +0000

    wip

--- haven/spark.py
+++ haven/spark.py
@@ -42,7 +42,7 @@ def configure(spark_session, region="", hadoop_version="3.3.4"):
         .config("spark.hadoop.fs.s3a.aws.credentials.provider", "com.amazonaws.auth.profile.ProfileCredentialsProvider")
         .config("spark.hadoop.fs.s3a.endpoint", f"s3.amazonaws.com")
         .config("spark.files", "https://s3.amazonaws.com/athena-downloads/drivers/JDBC/SimbaAthenaJDBC-2.0.33.1003/AthenaJDBC42-2.0.33.jar")
-        .config("spark.jars", "./AthenaJDBC42-2.0.33.jar")
+        #.config("spark.jars", "./AthenaJDBC42-2.0.33.jar")
         #.config("spark.jars", "https://s3.amazonaws.com/athena-downloads/drivers/JDBC/SimbaAthenaJDBC-2.0.33.1003/AthenaJDBC42-2.0.33.jar")
         #.config("spark.driver.extraClassPath", "./AthenaJDBC42-2.0.33.jar")
         #.config("spark.executor.extraClassPath", "./AthenaJDBC42-2.0.33.jar")
